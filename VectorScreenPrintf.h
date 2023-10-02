3e66463195112f924a4060a2fab80673b1b874f7
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2023-10-02 10:54:39 +0900

    hoge

--- VectorScreenPrintf.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include"MyVector2.h"
-#include"MyVector3.h"
-
-void VectorScreenPrintf(int x, int y, const MyVector2& vector, const char* text);
-
-void VectorScreenPrintf(int x, int y, const MyVector3& vector, const char* text);
