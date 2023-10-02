918d4c15382db9ee5f5937a3ad728f6fe6be040d
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2023-10-02 11:44:52 +0900

    01_01

---
+++ main.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+template <typename T>
+T min(T a,T b) {
+	if (a < b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+
+template <>
+char min<char>(char a, char b) {
+	printf("数字以外は代入できません");
+	return '\n';
+}
+
+int main() {
+
+	printf("%d\n", min<int>(114, 514));
+	printf("%f\n", min<float>(11.4f, 51.4f));
+	printf("%f\n", min(11.4f, 51.4f));
+	printf("%c", min<char>('114', '514'));
+
+	return 0;
+}
